36137c376f68fb095941867f58bc0139cc8e6384
Shatroopa Saxena <shatroopa-si@users.noreply.github.com>
2026-04-27 12:15:49 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ## nums is not necessarily sorted
+        # Crete hash map: {value: [indices in nums]}
+        nums_map = {}
+        i = 0               # iterator
+        while i < len(nums):
+            n = nums[i]
+            if n in nums_map:
+                nums_map[n].append(i)
+            else:
+                nums_map[n] = [i]
+            i += 1
+
+        # Search for sum of 2 numbers == target
+        i = 0               # iterator
+        while i < len(nums):
+            diff = target - nums[i]
+            
+            # Given that there is only one output pair
+            if diff in nums_map:
+                for j in nums_map[diff]:
+                    if j != i:
+                        return [i, j] if i < j else [j, i]
+            i += 1
+
+        
