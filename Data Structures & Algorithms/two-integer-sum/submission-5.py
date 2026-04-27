a658eb1db107b2c29310f17dee2d16c691c277be
Shatroopa Saxena <shatroopa-si@users.noreply.github.com>
2026-04-27 12:27:39 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ## nums is not necessarily sorted
+        # Create hash map: {value: most recent index in nums]}
+        nums_map = {}
+
+        # Search for sum of 2 numbers == target
+        i = 0               # iterator
+        while i < len(nums):
+            diff = target - nums[i]
+            
+            # Given that there is only one output pair
+            if diff in nums_map:
+                j = nums_map[diff]
+                if j != i:
+                    return [j, i]
+            
+            nums_map[nums[i]] = i
+            i += 1
+
+        
